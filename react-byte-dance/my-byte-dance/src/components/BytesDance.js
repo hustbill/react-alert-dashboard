4bf8d7d6304a63fff1201d88f05e159dad449b67
hua zhang <huazhang@visa.com>
2019-11-07 17:45:22 -0800

    ball with label works

--- react-byte-dance/my-byte-dance/src/components/BytesDance.js
+++ react-byte-dance/my-byte-dance/src/components/BytesDance.js
@@ -2,7 +2,6 @@ import React, {
   Component
 } from 'react';
 import * as d3 from "d3";
-import { defaultCipherList } from 'constants';
 
 class BytesDance extends Component {
   constructor(props) {
@@ -16,82 +15,92 @@ class BytesDance extends Component {
     }
   }
 
-  tick() {
-    var data = this.state.data;
-    this.count++;
-    data = data.reverse();
-    data = data.concat + " , count: " + this.count;
-    this.setState({ data });
-  }
-
   componentDidMount() {
-    // var data = this.state.data;
-    // console.log('componentDidMount data: ' + data); 
-    // data = data.concat('\n hello world');
-    // this.setState({data});
     this.timer = setTimeout(() => {
       this.setState({
         time: new Date()
       });
     }, Math.floor(Date.now() / 1000) * 1000 + 1000 - Date.now());
-
+    var balls = this.generateBallsWithLabel();
   }
 
   componentDidUpdate() {
-
+    
 
   }
 
   generateBallsWithLabel() {
-    var nodesJSONData = {
-      "nodes": [
-        { "x": 80, "r": 40, "label": "Node 1" },
-        { "x": 200, "r": 60, "label": "Node 2" },
-        { "x": 380, "r": 80, "label": "Node 3" }
-      ]
-    };
-
     var width = 960,
       height = 500;
 
-    var svg = d3.select("body").append("svg")
+    var svg = d3.select("#drawArea").append("svg")
       .attr("width", width)
       .attr("height", height)
 
+    var json = {
+      "nodes": [{
+        "x": 80,
+        "r": 30,
+        "color": "red",
+        "label": "Node1"
+      }, {
+        "x": 200,
+        "r": 45,
+          "color": "yellow",
+        "label": "Node2"
+      }, {
+        "x": 380,
+        "r": 60,
+          "color": "green",
+        "label": "Node3"
+      }]
+    };
+
     /* Define the data for the circles */
     var elem = svg.selectAll("g")
-      .data(nodesJSONData.nodes)
+      .data(json.nodes)
 
     /*Create and place the "blocks" containing the circle and the text */
     var elemEnter = elem.enter()
       .append("g")
-      .attr("transform", function (d) { return "translate(" + d.x + ",80)" })
+      .attr("transform", function (d) {
+        return "translate(" + d.x + ",80)"
+      })
 
     /*Create the circle for each block */
     var circle = elemEnter.append("circle")
-      .attr("r", function (d) { return d.r })
-      .attr("stroke", "black")
-      .attr("fill", "white")
+      .attr("r", function (d) {
+        return d.r
+      })
+      .attr("stroke", "black")      
+      .attr("fill", function (d) {
+        return d.color
+      });
 
     /* Create the text for each block */
     elemEnter.append("text")
-      .attr("dx", function (d) { return -20 })
-      .text(function (d) { return d.label });
-
-
+      .attr("dx", function (d) {
+        return -20
+      })
+      .text(function (d) {
+        return d.label
+      })
+    
     return circle;
   }
 
   render() {
     var data = this.state.timer.toLocaleTimeString();
-    var balls = this.generateBallsWithLabel();
+    // var balls = this.generateBallsWithLabel();
+    var drawAreaStyle = {
+      "width": 960,
+      "height":  500,      
+      "border": "1px solid grey"
+    }
     return (
-      <div id="drawArea">
-        <h1> Bytes dance randomly </h1>
-        <div ref="canvas" > {data} </div>
-        <div ref="BytesDance">
-          
-            </div>
+      <div id="drawArea" style={drawAreaStyle}>
+        <h1> Ball Bounce Randomly </h1>
+        <div ref="canvas" > {data} </div>            
       </div>
     );
   }
